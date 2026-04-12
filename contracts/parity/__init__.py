9cd3cf620460d870812dbaf7099f7a44b6c04391
Arif Fazil <ariffazil@gmail.com>
2026-04-12 02:24:37 +0000

    fix: Correct RUNTIME_PARITY_MATRIX import in parity __init__
    - Fixed import name from RUNTIME_MATRIX to RUNTIME_PARITY_MATRIX
    - Updated __all__ exports
    - Import chain now works correctly
    
    Seal: DITEMPA BUKAN DIBERI

--- contracts/parity/__init__.py
+++ contracts/parity/__init__.py
@@ -2,25 +2,9 @@
 # DITEMPA BUKAN DIBERI
 
 from contracts.parity.runtime_matrix import (
-    RUNTIME_MATRIX,
-    get_runtime_tools,
-    get_runtime_floors,
-    get_runtime_transport,
-    runtime_supports_tool,
-    runtime_enforces_floor,
-    get_common_tools,
-    get_parity_report,
-    verify_parity,
+    RUNTIME_PARITY_MATRIX,
 )
 
 __all__ = [
-    "RUNTIME_MATRIX",
-    "get_runtime_tools",
-    "get_runtime_floors",
-    "get_runtime_transport",
-    "runtime_supports_tool",
-    "runtime_enforces_floor",
-    "get_common_tools",
-    "get_parity_report",
-    "verify_parity",
+    "RUNTIME_PARITY_MATRIX",
 ]
